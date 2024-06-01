9abb23e17b26494a2d5eb372ad552ba8f6da0408
Rocket_Laggon <115794019+Abhinav2004arya@users.noreply.github.com>
2024-06-01 14:45:27 +0530

    Update CSV_Parsing.java

--- src/csv_parsing/CSV_Parsing.java
+++ src/csv_parsing/CSV_Parsing.java
@@ -4,48 +4,55 @@
  */
 package csv_parsing;
 
+// Add the apache common lang library and OpenCsv library
+//import necessary packages
+
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
-import com.opencsv.exceptions.CsvValidationException;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+
 
 /**
  *
  * @author ABHINAV ARYA
  */
+
+//This is the class for parsing CSV files
+
 public class CSV_Parsing {
     
     public static void main(String[] args) {
         try {
+            
+//            We will make use of OpenCsv's Reader and Writer methods to read the CSV file and write them in new CSV file
+            
             CSVReader csvReader = new CSVReader(new FileReader("e:/StockDataBANKBARODA.csv"));
+            CSVWriter csvWritter = new CSVWriter(new FileWriter("e:/rut.csv"));
             String[] lines;
+           
+            
             
             while ((lines = csvReader.readNext())!= null)
             {
                 System.out.println("Date: " + lines[1] + ", Opening Price: " + lines[2] + ", Highest Price: " + lines[3] 
                             + ", Lowest Price: " + lines[4] + ", Closing Price: " + lines[5] + ", Total Volume: " + lines[6] );
                 
-            CSVWriter csvWritter = new CSVWriter(new FileWriter("e:/resut.csv"));
-            String[] header = {"Date,Opening Price,Closing price"};
-            csvWritter.writeNext(header);
-            csvWritter.writeAll(csvReader);
                 
+                String[] WantedColumns = {lines[0],lines[1],lines[2],lines[5]};
+                csvWritter.writeNext(WantedColumns);
+  
             }
+            csvWritter.close();
+            System.out.println("Data Entered Succesfully!!!");
             
             
+           
             
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(CSV_Parsing.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(CSV_Parsing.class.getName()).log(Level.SEVERE, null, ex);
         } 
-        catch (CsvValidationException ex) {
-            Logger.getLogger(CSV_Parsing.class.getName()).log(Level.SEVERE, null, ex);
+        catch (Exception e) {
+            System.out.println("This Exception Occured: " + e);
         }
         
     }
